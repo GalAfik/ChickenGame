62c4a7c66456910e38a8942b2cdec646d7d3f2cf
Gal Afik <gal.p.afik@gmail.com>
2020-01-15 22:14:37 -0500

    Fixed egg decal rotation issue

--- Assets/Scripts/Egg.cs
+++ Assets/Scripts/Egg.cs
@@ -26,13 +26,19 @@ public class Egg : MonoBehaviour, Shootable
 		// Destroy this object when it hits the ground
 		if (collision.gameObject.name != playerObject.name)
 		{
+			Debug.Log(collision.gameObject.name);
+			// Spawn an instance of the egg splatter object that fades over time
+			Vector3 eggDecalHeightOffset = new Vector3(0, 0.01f, 0);
+			GameObject newDecalObject = Instantiate(
+				eggSplatterObject,
+				collision.GetContact(0).point + eggDecalHeightOffset,
+				Quaternion.FromToRotation(Vector3.forward, collision.GetContact(0).normal)
+				);
+			newDecalObject.transform.RotateAround(Vector3.zero, collision.GetContact(0).normal, Random.Range(0, 1)); // Randomly rotate new object
+			newDecalObject.transform.localScale *= Random.Range(1, 1.5f); // Randomly scale new object
+
 			// Destroy the egg
 			Destroy(gameObject);
-			// Spawn an instance of the egg splatter object that fades over time
-			Debug.Log(Quaternion.FromToRotation(Vector3.up, collision.contacts[0].normal));
-			GameObject newDecalObject = Instantiate(eggSplatterObject,collision.contacts[0].point, Quaternion.identity);
-			newDecalObject.transform.RotateAround(Vector3.zero, collision.contacts[0].normal, Random.Range(0, 1)); // Randomly rotate new object
-			newDecalObject.transform.localScale *= Random.Range(1, 1.5f); // Randomly scale new object
 		}
 	}
 
